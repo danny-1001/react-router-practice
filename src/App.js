165f86ac31287f369992cf6a664d814e0bad4383
Your Name <shazam1258@gmail.com>
2022-02-21 15:48:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./Styles/Styles.css";
+import Nav from "./Components/Nav";
+import About from "./Components/About";
+import Shop from "./Components/Shop";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Switch from "react-switch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav></Nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/shop" exact component={Shop} />
+      </Switch>
+    </Router>
   );
 }
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+);
 
 export default App;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return <div>
+      <nav>
+          <h3>LOGO</h3>
+          <ul className="nav-links">
+              <li>About</li>
+              <li>SHOP</li>
+          </ul>
+      </nav>
+  </div>;
+}
+
+export default Nav;
